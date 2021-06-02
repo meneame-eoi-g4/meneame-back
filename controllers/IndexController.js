94cbace76e0aadc6eeac18ee73e30ea0e2817c2c
vladikz7 <vladikz@hotmail.com>
2021-06-02 09:58:40 +0200

    Heroku mensaje de inicio cambiado

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.route('/')
   .get((req, res) => {
     const now = new Date()
-    const indexMessage = `API Pizza delicious v1.0.0, todos los derechos reservados ${now.getFullYear()}.`
+    const indexMessage = `Grupo 4 ${now.getFullYear()}.`
     res.send(indexMessage)
   })
 
