f73161ad8ca8f9e9710aa4d854b1680b06dae9eb
vladikz7 <vladikz@hotmail.com>
2021-06-02 10:22:15 +0200

    Esquema de articulo modificado

--- models/schemas/ArticleSchema.js
+++ models/schemas/ArticleSchema.js
@@ -4,14 +4,14 @@ const { Schema } = require("mongoose")
 const ArticleSchema = new Schema({
   title: { type: String, required: true, minlength: 3, maxlength: 255 },
   slug: { type: String, required: true, minlength: 3, maxlength: 255, unique: true },
-  image: { type: String, required: true },
+  img: { type: String, required: true },
   author: {
     name: {type: String, required: true, minlength: 3, maxlength: 200},
-    image: { type: String, required: true }
+    img: { type: String, required: true }
   },
   content: { type: String, required: true, minlength: 3 },
   published_at: {type: Date, default: Date.now},
-  category: { type: String, required: true, enum: ["Cosas"] },
+  categories: { type: String, required: true, enum: ["Cosas"] },
   enabled: { type: Boolean, default: true },
   positive: {type: Number, default: 0},
   negative: {type: Number, default: 0},
